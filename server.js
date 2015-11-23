3d9691ee9af8016cbeea6e797f4896c2734d0f1e
Soren Peterson <soren468@gmail.com>
2015-11-22 18:58:43 -0800

    run a simple docker command

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var exec	   = require('child_process').exec;
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -18,8 +19,13 @@ var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-	    res.json({ message: 'hooray! welcome to our api!' });   
+router.get('/run', function(req, res) {
+	exec('docker run node ls', function(err, stdout, stderr) {
+		res.json({
+			results: stdout,
+			algo: req.param('algo')
+		});
+	});
 });
 
 // more routes for our API will happen here
